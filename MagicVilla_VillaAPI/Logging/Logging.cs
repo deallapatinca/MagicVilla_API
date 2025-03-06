8515ffb7e75df6b4ef2d688dde01c5c329908066
deallapatinca <deallapatinca04@gmail.com>
2025-03-06 14:55:34 +0100

    Configuring API

---
+++ MagicVilla_VillaAPI/Logging/Logging.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MagicVilla_VillaAPI.Logging
+{
+    public class Logging : ILogging
+    {
+        public void Log(string message, string type)
+        {
+         
+           if(type == "error")
+           {
+                Console.WriteLine("ERROR - " + message);
+
+           }
+           else
+           {
+                Console.WriteLine(message);
+           }
+        }
+    }
+}
