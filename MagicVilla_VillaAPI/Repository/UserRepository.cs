ad316a074dbd9b85c3cb03a2571fd37e6c4196ea
deallapatinca <deallapatinca04@gmail.com>
2025-03-27 11:30:19 +0100

    User repository

---
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,61 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDBContext _db;
+        private string secretKey; 
+
+        public UserRepository(ApplicationDBContext db, IConfiguration configuration)
+        {
+            _db = db;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+            if (user == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            var user = _db.LocalUsers.FirstOrDefault(u => u.UserName == loginRequestDTO.UserName.ToLower() && u.Password == loginRequestDTO.Password);
+            if (user == null)
+            {
+                return null;
+            }
+
+            //if user was found generate JWT Token
+            var tokenHandle = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+        }
+
+        public async Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
+        {
+            LocalUser user = new LocalUser()
+            {
+                UserName = registrationRequestDTO.UserName,
+                Password = registrationRequestDTO.Password,
+                Name = registrationRequestDTO.Name,
+                Role = registrationRequestDTO.Role
+            };
+
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
+
+        }
+    }
+}
